60c06671e99f8e94c59bd1e615d46c0810c65777
tonasijko74-crypto <tonasijko74@gmail.com>
2026-05-29 11:06:11 +0300

    Edited ships color

--- ships.py
+++ ships.py
@@ -6,28 +6,30 @@ class Ship:
         self.x = x
         self.y = y
         self.size = size
-        self.cell=20
-        self.color=(0, 0, 0)
+        self.cell=30
+        self.color="BROWN"
         self.dragg=False
         self.offset_x = 0
         self.offset_y = 0
-        self.angle=90
+        self.angle=0
         width = self.cell
         height = self.cell * size
-        self.original_surface = pygame.Surface((width + 20, height + 20))
+        self.original_surface = pygame.Surface((width + 10, height + 10))
         self.change = self.original_surface
         self.rect = self.change.get_rect(center=(self.x, self.y))
     def draw(self):
+        self.original_surface.fill('WHITE')
         pygame.draw.rect(self.original_surface, self.color,(10, 20, self.cell, self.cell * (self.size - 1)))
-        pygame.draw.polygon(self.original_surface, self.color,[(10, 20), (20, 0), (30, 20)])
         screen.blit(self.change, self.rect) # to combine to surfaces basically
     def drag(self, event):
         if event.type==pygame.MOUSEBUTTONDOWN:
             if event.button==1:
-                self.dragg=True
-                mouse_x, mouse_y = event.pos
-                self.offset_x = self.rect.x - mouse_x
-                self.offset_y = self.rect.y - mouse_y
+                # FIX: Added collidepoint check so you only drag when clicking ON the ship
+                if self.rect.collidepoint(event.pos):
+                    self.dragg=True
+                    mouse_x, mouse_y = event.pos
+                    self.offset_x = self.rect.x - mouse_x
+                    self.offset_y = self.rect.y - mouse_y
 
         elif event.type ==pygame.MOUSEBUTTONUP:
             if event.button == 1:
@@ -50,8 +52,23 @@ class Ship:
                 )
                 self.rect = self.change.get_rect(center=center)
 
+    def get_cells(self):
+        cells = []
+        center_x, center_y = self.rect.center
 
-    # def killed(self):w
+        if self.angle in [0, 180]:
+            start_x = center_x // self.cell
+            start_y = (center_y - (self.size * self.cell) // 2) // self.cell
+            for i in range(self.size):
+                cells.append((start_x, start_y + i))
+
+        elif self.angle in [90, 270]:
+            start_x = (center_x - (self.size * self.cell) // 2) // self.cell
+            start_y = center_y // self.cell
+            for i in range(self.size):
+                cells.append((start_x + i, start_y))
+
+        return cells
 
 class Ship1(Ship):
     def __init__(self, x, y):
@@ -79,6 +96,7 @@ while running: # output ship
             running = False
         ship.drag(event)
         ship.turn(event)
+        ship.get_cells()
     screen.fill((255, 255, 255)) # white screen
     ship.draw()
     pygame.display.update()
