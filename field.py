4e48cbcce3c918b5af481ab63bf79b8f53226271
Uliana <u.trubach08@gmail.com>
2026-05-29 23:01:26 +0300

    added shot function and killed function

--- field.py
+++ field.py
@@ -1,20 +1,48 @@
 import pygame as pg
+pg.init()
+screen = pg.display.set_mode((800, 800))
 class Field:
     def __init__(self):
+        self.ships = []
         self.shots = {}
     def draw(self, screen):
         for row in range(10):
             for col in range(10):
-                rectangle = pg.Rect(100+col*50, 100+row*50, 50, 50)
-                pg.draw.rect(screen, 'BLUE', rectangle)
+                rectangle = pg.Rect(70+col*30, 70+row*30, 30, 30)
+                pg.draw.rect(screen, 'WHITE', rectangle)
                 pg.draw.rect(screen, "BLACK", rectangle, 1)
                 if (col, row) in self.shots:
-                    hit = self.shots[(col, row)]
-                    color = 'RED' if hit else 'BLUE'
-                    pg.draw.rect(screen, color, rectangle)
+                    state = self.shots[(col, row)]
+                    if state == 'missed':
+                        pg.draw.circle(screen, 'BLUE', rectangle.center, 5)
+                    elif state == 'hit':
+                        pg.draw.circle(screen, 'RED', rectangle.center, 8)
+                    elif state == 'killed':
+                        pg.draw.rect(screen, 'BLACK', rectangle)
 
     def inside_board(self, x, y):
         return 0 <= x < 10 and 0 <= y < 10
-    #def positions(self, ship):
 
-a = Field()
+    def shot(self, mouse_pos):
+        mx, my = mouse_pos
+        gx = (mx - 70) // 30
+        gy = (my - 70) // 30
+        if not self.inside_board(gx, gy):
+            return 'missed'
+        if (gx, gy) in self.shots and self.shots[(gx, gy)] == 'killed':
+            return 'killed'
+        result = 'missed'
+        for ship in self.ships:
+            if (gx, gy) in ship.get_cells():
+                result = 'hit'
+                ship.hit_cells.add((gx, gy))
+                if ship.get_killed():
+                    result = 'killed'
+                    for cell in ship.get_cells():
+                        self.shots[cell] = 'killed'
+                else:
+                    self.shots[(gx, gy)] = 'hit'
+                break
+        if result == 'missed':
+            self.shots[(gx, gy)] = 'missed'
+        return result
