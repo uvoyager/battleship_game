1da16196a00c1a3b8dfffe56a10c164a6aa94247
Uliana <u.trubach08@gmail.com>
2026-05-29 23:00:47 +0300

    added snap function and changed positining system

--- ships.py
+++ ships.py
@@ -1,85 +1,82 @@
 import pygame
-pygame.init()
-screen = pygame.display.set_mode((400, 400))
 class Ship:
     def __init__(self, x, y, size):
-        self.x = x
-        self.y = y
         self.size = size
-        self.cell=20
-        self.color=(0, 0, 0)
-        self.dragg=False
+        self.cell = 30
+        self.color = 'BROWN'
+        self.dragg = False
         self.offset_x = 0
         self.offset_y = 0
-        self.angle=90
+        self.angle = 0
+        self.board_offset = 70
+        self.col = (x - self.board_offset) // self.cell
+        self.row = (y - self.board_offset) // self.cell
         width = self.cell
         height = self.cell * size
-        self.original_surface = pygame.Surface((width + 20, height + 20))
+        self.original_surface = pygame.Surface((width, height))
+        self.original_surface.fill(self.color)
         self.change = self.original_surface
-        self.rect = self.change.get_rect(center=(self.x, self.y))
-    def draw(self):
-        pygame.draw.rect(self.original_surface, self.color,(10, 20, self.cell, self.cell * (self.size - 1)))
-        pygame.draw.polygon(self.original_surface, self.color,[(10, 20), (20, 0), (30, 20)])
-        screen.blit(self.change, self.rect) # to combine to surfaces basically
+        self.rect = self.change.get_rect()
+        self.snap_to_grid()
+        self.hit_cells = set()
+    def snap_to_grid(self):
+        pixel_x = self.board_offset + self.col * self.cell
+        pixel_y = self.board_offset + self.row * self.cell
+        if self.angle in [0, 180]:
+            self.rect = self.change.get_rect(topleft=(pixel_x, pixel_y))
+        else:
+            self.rect = self.change.get_rect(topleft=(pixel_x, pixel_y))
+    def draw(self, screen):
+        screen.blit(self.change, self.rect)
     def drag(self, event):
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            if event.button==1:
-                self.dragg=True
-                mouse_x, mouse_y = event.pos
-                self.offset_x = self.rect.x - mouse_x
-                self.offset_y = self.rect.y - mouse_y
-
-        elif event.type ==pygame.MOUSEBUTTONUP:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
+                if self.rect.collidepoint(event.pos):
+                    self.dragg = True
+                    mouse_x, mouse_y = event.pos
+                    self.offset_x = self.rect.x - mouse_x
+                    self.offset_y = self.rect.y - mouse_y
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1 and self.dragg:
                 self.dragg = False
-
-        elif event.type ==pygame.MOUSEMOTION:
+                self.col = round((self.rect.x - self.board_offset) / self.cell)
+                self.row = round((self.rect.y - self.board_offset) / self.cell)
+                self.col = max(0, min(self.col, 10 - (self.size if self.angle in [90, 270] else 1)))
+                self.row = max(0, min(self.row, 10 - (1 if self.angle in [90, 270] else self.size)))
+                self.snap_to_grid()
+        elif event.type == pygame.MOUSEMOTION:
             if self.dragg:
                 mouse_x, mouse_y = event.pos
                 self.rect.x = mouse_x + self.offset_x
                 self.rect.y = mouse_y + self.offset_y
-
     def turn(self, event):
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_d:
-                self.angle += 90
-                center = self.rect.center
-                self.change = pygame.transform.rotate(
-                    self.original_surface,
-                    self.angle
-                )
-                self.rect = self.change.get_rect(center=center)
+                if self.dragg or self.rect.collidepoint(pygame.mouse.get_pos()):
+                    self.angle = (self.angle + 90) % 360
+                    self.change = pygame.transform.rotate(self.original_surface, self.angle)
+                    self.snap_to_grid()
+    def get_cells(self):
+        cells = []
+        if self.angle in [0, 180]:
+            for i in range(self.size):
+                cells.append((self.col, self.row + i))
+        elif self.angle in [90, 270]:
+            for i in range(self.size):
+                cells.append((self.col + i, self.row))
+        return cells
+    def get_killed(self):
+        for cell in self.get_cells():
+            if cell not in self.hit_cells:
+                return False
+        return True
 
-
-    # def killed(self):w
 
 class Ship1(Ship):
-    def __init__(self, x, y):
-        Ship.__init__(self, x, y, 1)
-
+    def __init__(self, x, y): super().__init__(x, y, 1)
 class Ship2(Ship):
-    def __init__(self, x, y):
-        Ship.__init__(self, x, y, 2)
-
+    def __init__(self, x, y): super().__init__(x, y, 2)
 class Ship3(Ship):
-    def __init__(self, x, y):
-        Ship.__init__(self, x, y, 3)
-
+    def __init__(self, x, y): super().__init__(x, y, 3)
 class Ship4(Ship):
-    def __init__(self, x, y):
-        Ship.__init__(self, x, y, 4)
-
-
-### TEST SHIP
-ship = Ship(200, 100, 4)
-running = True
-while running: # output ship
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        ship.drag(event)
-        ship.turn(event)
-    screen.fill((255, 255, 255)) # white screen
-    ship.draw()
-    pygame.display.update()
-pygame.quit()
+    def __init__(self, x, y): super().__init__(x, y, 4)
